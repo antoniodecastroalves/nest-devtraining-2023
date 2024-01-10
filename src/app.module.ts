e0b6d343852b173caa6f9ea78ac34b6006ce1734
Antonio Alves <antoniodecastroalves@gmail.com>
2024-01-09 23:32:43 -0300

    :refactor: resolved conflicts in courses.module.ts and app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
-import { CoursesController } from './courses/courses.controller';
-import { CoursesService } from './courses/courses.service';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CoursesController],
-  providers: [AppService, CoursesService],
-=======
-import { CoursesModule } from './courses/courses.module';
-
-@Module({
->>>>>>> feat-introduction
   imports: [CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-<<<<<<< HEAD
 export class AppModule {}
-=======
-export class AppModule {}
->>>>>>> feat-introduction
