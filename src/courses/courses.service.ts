15a1168711e5e93514494c44ec31fc00522c85ff
Antonio Alves <antoniodecastroalves@gmail.com>
2024-01-16 23:36:15 -0300

    :sparkles: feat: Refactoring the Create and Update methods with relationships

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -3,13 +3,19 @@ import { Course } from './entities/courses.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Tag } from './entities/tags.entity';
+import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
 
 @Injectable()
 export class CoursesService {
   constructor(
     @InjectRepository(Course)
     private readonly courseRepository: Repository<Course>,
+  
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
     ) {}
+
   
   async findAll() {
     return this.courseRepository.find();
@@ -25,15 +31,26 @@ export class CoursesService {
     return course;
   }
 
-  async create(createCourseDTO: any) {
-    const course = this.courseRepository.create(createCourseDTO)
-    return this.courseRepository.save(course);
+  async create(createCourseDTO: CreateCourseDTO) {
+    const tags = await Promise.all(
+      createCourseDTO.tags.map(name => this.preloadTagByName(name)),
+    )
+    const course = this.courseRepository.create({
+      ...createCourseDTO,
+      tags,
+    })
+    return this.courseRepository.save(course)
   }
 
-  async update(id: number, updateCourseDTO: any) {
+  async update(id: number, updateCourseDTO: UpdateCourseDTO) {
+    const tags = updateCourseDTO.tags && (await Promise.all(
+      updateCourseDTO.tags.map(name => this.preloadTagByName(name)),
+    ))
+
     const course = await this.courseRepository.preload({
       ...updateCourseDTO,
       id,
+      tags,
     });
 
     if(!course) {
@@ -51,5 +68,13 @@ export class CoursesService {
       throw new NotFoundException(`Course ID ${id} not found`)
     }
     return this.courseRepository.remove(course);
+  }
+
+  private async preloadTagByName(name: string): Promise<Tag> {
+    const tag = await this.tagRepository.findOne({ where: { name } })
+    if (tag) {
+      return tag
+    }
+    return this.tagRepository.create({ name })
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-    }));
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();
