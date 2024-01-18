ddefc5e0d06e5346d722129fb5b7bbaa00918acc
Antonio Alves <antoniodecastroalves@gmail.com>
2024-01-18 20:02:28 -0300

    :sparkles: feat: Creating the tags table through migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,11 +1,12 @@
 import { DataSource } from "typeorm";
 import { dataSourceOptions } from "./database.module";
 import { CreateCoursesTable1705612803344 } from './../migrations/1705612803344-CreateCoursesTable';
+import { CreateTagsTable1705617261348 } from './../migrations/1705617261348-CreateTagsTable';
 
 
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1705612803344]
+  migrations: [CreateCoursesTable1705612803344, CreateTagsTable1705617261348],
 })
